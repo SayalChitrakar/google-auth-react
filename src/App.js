866c9834f49678f202b0ce580a93a43a5ac3cdc1
SayalChitrakar <chitrakar.sayal@gmail.com>
2023-01-27 10:59:19 +0545

    initial push.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import axios from "axios";
 
 function App() {
+  async function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token" + response.credential);
+    console.log(jwt_decode(response.credential));
+
+    const a = await axios
+      .post("http://localhost:4200/seller/login-google", {
+        token: response.credential,
+      })
+      .then((data) => {
+        console.log(data);
+      });
+    console.log(`this is the data ${a.data}`);
+  }
+  useEffect(() => {
+    /* global google*/
+    google.accounts.id.initialize({
+      client_id:
+        "879627649201-k5lm13it0koe5giks81jf116fl3e7bo6.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="signInDiv"></div>
     </div>
   );
 }
